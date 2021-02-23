0cb5b396d644b9497df8c65960377cebcecfb979
Sara Tolstrup Husum <sarathusum@gmail.com>
2021-02-23 21:59:55 +0100

    separating CohortItemCard from CohortList

--- zeeguu-teacher-dashboard/src/components/CohortList.js
+++ zeeguu-teacher-dashboard/src/components/CohortList.js
@@ -1,39 +1,12 @@
 import { Button, Dialog, DialogContent } from '@material-ui/core'
-import { Link } from '@reach/router'
 import React, { useState } from 'react'
-import { MdAddCircle, MdArrowForward, MdPeople } from 'react-icons/md/'
+import { MdAddCircle } from 'react-icons/md/'
 import { createCohort } from '../api/apiCohort'
 import CohortForm from './CohortForm'
+import { CohortItemCard }from './CohortItemCard'
 import { toast } from 'react-toastify'
 
 import '../assets/styles/components/cohortList.scss'
-
-const CohortItem = ({ cohort }) => {
-  return (
-    <div className="cohort-card">
-      <div>
-        <p>{cohort.language_name}</p>
-        <p className="special">
-          {cohort.cur_students}/{cohort.max_students}{' '}
-          <MdPeople className="cohort-card-icon-people" size="22px" />
-        </p>
-      </div>
-      <h2 className="cohort-card-headline">{cohort.name}</h2>
-      <div>
-        <p className="font-light">invite code: {cohort.inv_code}</p>
-        <Button
-          variant="contained"
-          color="primary"
-          component={Link}
-          to={`/${process.env.REACT_APP_ROOT_NAME}/classroom/${cohort.id}`}
-        >
-          View class
-          <MdArrowForward className="cohort-card-btn-arrow" size="18px" />
-        </Button>{' '}
-      </div>
-    </div>
-  )
-}
 
 const CohortList = ({ cohorts, refetch }) => {
   const [isOpen, setIsOpen] = useState(false)
@@ -60,7 +33,7 @@ const CohortList = ({ cohorts, refetch }) => {
   return (
     <div className="cohorts-list">
       {cohorts.map(cohort => (
-        <CohortItem key={cohort.id} cohort={cohort} />
+        <CohortItemCard key={cohort.id} cohort={cohort} />
       ))}
       <Button
         aria-label="Create Class"
